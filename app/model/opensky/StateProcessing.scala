5e2844e8e21351ed108e42310e4638e0163abfc5
renghen <renghen@yahoo.com>
2023-06-19 11:37:06 +0400

    Minor refactoring

--- app/model/opensky/StateProcessing.scala
+++ app/model/opensky/StateProcessing.scala
@@ -7,17 +7,34 @@ import scala.collection.mutable
 import javax.inject.{Inject, Singleton}
 import java.time.Instant
 
+object StateProcessing {
+  type Icao24 = String
+  type Country = String
+}
+
 @Singleton
 class StateProcessing @Inject() (implicit ec: ExecutionContext) {
+  import StateProcessing._
   final val delay: Int = 3600
-  val countries: mutable.HashMap[String, Int] = mutable.HashMap.empty[String, Int]
-  val planeAboveNetherlands: mutable.HashMap[String, Long] = mutable.HashMap.empty[String, Long]
+  private val listOfIcao24 = mutable.ArrayBuffer.empty[Icao24]
+  private val countryOrigin: mutable.HashMap[String, Int] = mutable.HashMap.empty[Country, Int]
+  private val planeAboveNetherlands: mutable.HashMap[String, Long] = mutable.HashMap.empty[Icao24, Long]
 
   def toCountries(state: State): Unit = {
-    countries.get(state.originCountry) match {
-      case None        => countries.addOne((state.originCountry, 1))
-      case Some(value) => countries.update(state.originCountry, value + 1)
+    val isFound = listOfIcao24.find(code => code == state.icao24) // we check for unique planes
+    isFound match {
+      case None =>
+        listOfIcao24.addOne(state.icao24)
+        countryOrigin.get(state.originCountry) match {
+          case None        => countryOrigin.addOne((state.originCountry, 1))
+          case Some(value) => countryOrigin.update(state.originCountry, value + 1)
+        }
+      case Some(_) => // we do not do anything
     }
+  }
+
+  def getCountries(): Map[String, Int] = {
+    countryOrigin.toMap
   }
 
   def isAboveNetherlands(state: State): Boolean = {
@@ -43,6 +60,10 @@ class StateProcessing @Inject() (implicit ec: ExecutionContext) {
       val toRemove = planeAboveNetherlands.view.filter(pred => pred._2 > oneHourAgo).keys
       planeAboveNetherlands.subtractAll(toRemove)
     }
+  }
+
+  def aboveNetherlands() = {
+    planeAboveNetherlands.toMap
   }
 
   def processState(state: State) = {
